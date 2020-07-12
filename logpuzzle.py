fd14a69300fb535aa0e5acfbda3710f2e094f782
Kathryn Anderson <kat42272@yahoo.com>
2020-07-11 17:39:17 -0700

    Updated project contributors

--- logpuzzle.py
+++ logpuzzle.py
@@ -13,7 +13,7 @@ Here's what a puzzle URL looks like (spread out onto multiple lines):
 HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
-
+# written by Kathryn Anderson with the help of a private tutor
 import os
 import re
 import sys
